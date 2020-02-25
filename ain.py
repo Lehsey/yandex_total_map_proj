6dbb83a564e4ea5b4f2d7231f1242b5445ef8896
Leshey <Lahasek@mail.ru>
2020-02-25 19:16:16 +0300

    try to add mark

--- ain.py
+++ ain.py
@@ -5,20 +5,28 @@ import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QGuiApplication
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit
-
+from useful_defs import toponyms_searcher, spn_serch
 
 SCREEN_SIZE = [600, 550]
 MAP_TYPES = ['map', 'sat', 'sat,skl']
 
 
-def map_request(coords, z, map_type):
+def map_request(coords, z, map_type, marks, spn=False):
     api_server = 'https://static-maps.yandex.ru/1.x'
-    print(map_type)
-    params = {
-        'l': map_type,
-        'll': ','.join([str(el) for el in coords]),
-        'z': z
-    }
+    if not spn:
+        params = {
+            'l': map_type,
+            'll': ','.join([str(el) for el in coords]),
+            'z': z
+            } 
+    else:
+        params = {
+            'l': map_type,
+            'll': ','.join([str(el) for el in coords]),
+            'z': z,
+            'spn': spn,
+            'pt': ','.join([str(el) for el in marks])
+            }    
     response = requests.get(api_server, params=params)
     return response
 
@@ -29,11 +37,28 @@ class Main_window(QWidget):
         self.map = 'map.png'
         self.map_type = 0
         self.z = 8
+        self.mark = 0
         self.initUI()
 
     def getImage(self):
-        self.coords = [float(el) for el in self.input.text().split()]
-        map_resp = map_request(self.coords, self.z, MAP_TYPES[self.map_type])
+        try:
+            self.coords = [float(el) for el in self.input.text().split()]
+            if self.mark:
+                map_resp = map_request(self.coords, self.z, MAP_TYPES[self.map_type], self.mark)
+            else:
+                map_resp = map_request(self.coords, self.z, MAP_TYPES[self.map_type], self.coords)
+
+        except:
+            self.coords = self.input.text()
+            response = toponyms_searcher(self.coords)
+            need_city = response["response"]["GeoObjectCollection"]["featureMember"][0]
+            need_coord = need_city['Point']['pos'].split()
+            self.coords = need_coord
+            self.mark = need_coord
+            spn = spn_serch(need_city)
+            map_resp = map_request(self.coords, self.z, MAP_TYPES[self.map_type], self.mark, spn=spn)
+            
+
         if not requests:
             print(map_resp)
             sys.exit(1)
@@ -63,8 +88,8 @@ class Main_window(QWidget):
                 self.getImage()
                 self.input.clearFocus()
             
-            elif e.key() in (Qt.Key_PageUp, Qt.Key_PageDown) or (Qt.Key_Up, Qt.Key_Down, Qt.Key_Left, Qt.Key_Right, Qt.Key_L):
-                print(Qt.Key_L, e.key())
+            elif e.key() in (Qt.Key_PageUp, Qt.Key_PageDown, Qt.Key_Up, Qt.Key_Down, Qt.Key_Left, Qt.Key_Right, Qt.Key_L):
+                print(e.key(), Qt.Key_PageUp, Qt.Key_PageDown, Qt.Key_Up, Qt.Key_Down, Qt.Key_Left, Qt.Key_Right, Qt.Key_L)
                 if e.key() == Qt.Key_PageUp:
                     if self.z < 17:
                         self.z += 1
@@ -91,8 +116,7 @@ class Main_window(QWidget):
                 
                 elif e.key() == Qt.Key_L:
                         self.map_type = (self.map_type + 1) % 3
-                        print(self.map_type)
-                print(self.map_type)
+
                 
                 self.input.setText(' '.join([str(el) for el in self.coords]))
                 self.getImage()
---
+++ useful_defs.py
@@ -0,0 +1,51 @@
+import math
+import requests
+
+# Определяем функцию, считающую расстояние между двумя точками, заданными координатами
+def lonlat_distance(a, b):
+
+    degree_to_meters_factor = 111 * 1000 # 111 километров в метрах
+    a_lon, a_lat = a
+    b_lon, b_lat = b
+
+    # Берем среднюю по широте точку и считаем коэффициент для нее.
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    lat_lon_factor = math.cos(radians_lattitude)
+
+    # Вычисляем смещения в метрах по вертикали и горизонтали.
+    dx = abs(a_lon - b_lon) * degree_to_meters_factor * lat_lon_factor
+    dy = abs(a_lat - b_lat) * degree_to_meters_factor
+
+    # Вычисляем расстояние между точками.
+    distance = math.sqrt(dx * dx + dy * dy)
+
+    return distance
+
+def spn_serch(toponym):
+    envelope = toponym["boundedBy"]["Envelope"]
+
+    # левая, нижняя, правая и верхняя границы из координат углов:
+    l, b = envelope["lowerCorner"].split(" ")
+    r, t = envelope["upperCorner"].split(" ")
+
+    # Вычисляем полуразмеры по вертикали и горизонтали
+    dx = abs(float(l) - float(r)) / 2.0
+    dy = abs(float(t) - float(b)) / 2.0
+
+    # Собираем размеры в параметр span
+    span = "{dx},{dy}".format(**locals())
+    return span
+
+def toponyms_searcher(toponym):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params).json()
+    return response
+
+if __name__ == "__main__":
+    print(toponyms_searcher("Калуга"))
